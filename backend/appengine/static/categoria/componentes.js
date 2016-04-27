fc836ff3009d43e1bc8dcb2346f8bd58f4b1e12f
renzon <renzon@gmail.com>
2016-04-27 07:20:43 -0300

    Diretiva com função de salvar

--- backend/appengine/static/categoria/componentes.js
+++ backend/appengine/static/categoria/componentes.js
@@ -3,7 +3,16 @@ var categoriaComponentes = angular.module('categoria-componentes', []);
 categoriaComponentes.directive('categoriaForm', function () {
   return {
     restrict: 'E',
-    templateUrl:'/static/categoria/form.html'
+    templateUrl:'/static/categoria/form.html',
+    replace: true,
+    scope:{},
+    controller:function($scope){
+      $scope.categoria={nome:'Notebook', codigo:1};
+      $scope.salvar=function(){
+        console.log($scope.categoria);
+      }
+
+    }
   };
 });
 
